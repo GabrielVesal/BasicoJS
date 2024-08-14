9761c436a5f861904245a10d2f1a4ca514bda8eb
Gabriel   A.  Teixeira <132387258+GabrielVesal@users.noreply.github.com>
2024-08-14 13:58:13 -0300

    Add files via upload

---
+++ study.js
@@ -0,0 +1,49 @@
+////Tipos de variáveis:
+//let idade = 10;
+//let nome = "Gabriel";
+//let logado = false;
+//
+////array e objetos
+//let ingredientes = ["farinha", "agua", "sal"];
+//let personagem = {
+//  nome: "fulano",
+//  nivel: 10,
+//  forca: 800,
+//  magia: 200,
+//  vida: 1000,
+//  mana: 200,
+//};
+////console.log(ingredientes[0]);
+////console.log(personagem.nome);
+//
+////função
+//function somar(a, b) {
+//  let resultado = a + b;
+//  return resultado;
+//}
+//
+//let x = somar(3, 2);
+////console.log(x);
+//
+//let somar2 = (a, b) => {
+//  let resultado = a + b;
+//  return resultado;
+//};
+//
+//let y = somar2(3, 2);
+////console.log(y);
+
+//condição
+//let idade = 17;
+//if (idade >= 18) {
+//  console.log("Você é maior de idade!");
+//} else {
+//  console.log("Você e menor de idade!");
+//}
+
+//loop
+let lista = [10, 20, 30, 40];
+
+for (let item of lista) {
+  console.log("-Numero: " + item);
+}
